2d4c491b3a8bf2f402e3a551430cd286b5bc168e
valeria9630 <valequim96@gmil.com>
2018-05-19 22:34:42 -0600

    Arreglo

--- Tarea5/src/domain/Pruebas.java
+++ Tarea5/src/domain/Pruebas.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tarea5;
+package domain;
 
 import java.io.IOException;
 import java.util.ArrayList;
