76c5a8b9b05e80fd067bcf7fd3f064824f55ccef
danii230 <37677168+danii230@users.noreply.github.com>
2018-05-20 03:05:22 -0500

    sad
    dsa

--- Tarea5P/src/tarea5p/FXMLDocumentController.java
+++ Tarea5P/src/tarea5p/FXMLDocumentController.java
@@ -6,11 +6,16 @@
 package tarea5p;
 
 import java.net.URL;
+
+
+
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
 /**
@@ -19,8 +24,19 @@ import javafx.scene.layout.AnchorPane;
  */
 public class FXMLDocumentController implements Initializable {
     
+    
     @FXML
     private AnchorPane pane;
+//
+//    @FXML
+//    private ImageView imagen1;
+//
+//    @FXML
+//    private ImageView imagen2;
+//
+//    @FXML
+//    private ImageView imagen3;
+//    
     
 
     
@@ -31,6 +47,14 @@ public class FXMLDocumentController implements Initializable {
                 + "-fx-background-position: left top, center;"
                 + "-fx-background-repeat: no-repeat;"
                 + "-fx-background-size: cover, auto;");
+        
+//        Image imag = new Image("/imagen/table-2715994_960_720.png");
+//        imagen1 = new ImageView(imag);
+//        imagen2 = new ImageView(imag);
+//        imagen3 = new ImageView(imag);
+        
+       // pane.getChildren().addAll(imagen1);
+        
     }    
     
 }
